1d6efca9bdfbc5546834a9ab768f501f48e98fbe
Mazen Fathy <mazenfathy841@gmail.com>
2026-05-23 16:19:15 +0300

    fixing a bug faced me during testing the project in deploy stage

--- src/getData.js
+++ src/getData.js
@@ -3,7 +3,8 @@ const isInlocalStorage = JSON.parse(localStorage.getItem("data"))||[];
 if (isInlocalStorage.length > 0) {
   let i = 0;
   const newData = data.map((element) => {
-    if (element.name == isInlocalStorage[i].name) {
+    
+    if (isInlocalStorage[i]&&element.name == isInlocalStorage[i].name) {
       const newEle = {
         ...element,
         isActive:isInlocalStorage[i].isActive
